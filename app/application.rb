7929a34eb6ad99573bf540be90903aa18d726415
Nicky Liu <nickyliu91@gmail.com>
2018-09-17 06:13:47 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,10 +8,14 @@ class Application
      item_name = req.path.split("/items/").last
      item = @@items.find{|i| i.name == item_name}
 
+      if @@item.include?(item_name)
         resp.write item.price
-
+      else
+        resp.write "Item not found"
+        resp.status route = 400
+      end
     else
-      resp.write "Item not found"
+      resp.write "Route not found"
       resp.status = 404
     end
 

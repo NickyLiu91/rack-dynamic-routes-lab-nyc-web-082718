01062a01ffaf419d1e156389b08feeda5298103d
Nicky Liu <nickyliu91@gmail.com>
2018-09-17 06:20:31 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
      item_name = req.path.split("/items/").last
      item = @@items.find{|i| i.name == item_name}
 
-      if @@items.include?(item)
+      if item
         resp.write item.price
       else
         resp.write "Item not found"

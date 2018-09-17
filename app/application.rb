f53edb8bdfa5a452cce1a7f64f60e10c56200740
Nicky Liu <nickyliu91@gmail.com>
2018-09-17 06:17:22 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,8 +6,8 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path=="/items/<ITEM NAME>"
-     item_name = req.path.split("/items/<ITEM NAME>").last
+    if req.path.match("/items/")
+     item_name = req.path.split("/items/").last
      item = @@items.find{|i| i.name == item_name}
 
       if @@item.include?(item)

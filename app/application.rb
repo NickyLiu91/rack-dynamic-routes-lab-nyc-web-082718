0a18855f2a1776494ad1403f5d782da205fee274
Nicky Liu <nickyliu91@gmail.com>
2018-09-17 06:12:41 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,14 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path=="/items"
-      resp.write "You requested the items"
+     item_name = req.path.split("/items/").last
+     item = @@items.find{|i| i.name == item_name}
+
+      if @@item.include?(item_name)
+        resp.write item.price
+      else
+        resp.status route = 400
+      end
     else
       resp.write "Route not found"
       resp.status = 404
